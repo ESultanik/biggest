9180a6e84687e77f2a7072402e6bf58605203994
Evan Sultanik <evan@sultanik.com>
2018-11-03 22:36:03 -0400

    Improved the heap implementation to support successive removals before iteration

--- biggest/heap.py
+++ biggest/heap.py
@@ -1,57 +1,92 @@
 import heapq
 
+class MutableHeapElement(object):
+    def __init__(self, element, removed=False):
+        self.element = element
+        self.removed = removed
+
+    def __lt__(self, other):
+        return self.element < other.element
+
 class MutableHeap(object):
     def __init__(self, elements=None):
-        self._removed = set()
+        self._elements = {}
         if elements is None:
             self._heap = []
         else:
-            self._heap = list(elements)
+            self._heap = list(MutableHeapElement(element) for element in elements)
+            for e in self._heap:
+                if e.element in self._elements:
+                    self._elements[e.element].append(e)
+                else:
+                    self._elements[e.element] = [e]
         heapq.heapify(self._heap)
 
     def remove(self, element):
-        self._removed.add(element)
-        self._clear_removed()
+        if element in self._elements:
+            for e in self._elements[element]:
+                e.removed = True
+            del self._elements[element]
+            self._clear_removed()
+            return True
+        else:
+            return False
 
     def _clear_removed(self):
-        while self._heap and self._heap[0] in self._removed:
+        while self._heap and self._heap[0].removed:
             heapq.heappop(self._heap)
 
     def __len__(self):
-        return sum(1 for e in self._heap if e not in self._removed)
+        return sum(1 for e in self._heap if not e.removed)
 
     def __bool__(self):
         return bool(self._heap)
 
     def __contains__(self, element):
-        return element in self._heap
-    
+        return element in self._elements and sum(map(lambda e : not e.removed, self._elements[element]))
+
     def peek(self):
         if not self._heap:
             raise LookupError('The heap is empty')
-        return self._heap[0]
-        
+        return self._heap[0].element
+
     def pop(self):
         while self._heap:
             ret = heapq.heappop(self._heap)
-            if ret not in self._removed:
+            if not ret.removed:
                 self._clear_removed()
-                return ret
+                self._elements[ret.element].remove(ret)
+                if not self._elements[ret.element]:
+                    del self._elements[ret.element]
+                return ret.element
         raise LookupError('The heap is empty')
 
     def push(self, element):
-        heapq.heappush(self._heap, element)
+        e = MutableHeapElement(element)
+        if element in self._elements:
+            self._elements[element].append(e)
+        else:
+            self._elements[element] = [e]
+        heapq.heappush(self._heap, e)
 
 if __name__ == '__main__':
     import random
-    to_remove = frozenset(random.randint(0, 100) for i in range(20))
+    to_remove = frozenset(random.randint(0, 99) for i in range(20))
     print(f"Removed: {', '.join(map(str, sorted(to_remove)))}")
     h = MutableHeap()
     for i in reversed(range(100)):
         h.push(i)
     for i in to_remove:
-        h.remove(i)
+        assert h.remove(i)
     results = []
     while h:
         results.append(h.pop())
     print(f"Result: {', '.join(map(str, results))}")
+    h = MutableHeap([1, 2, 3])
+    h.remove(2)
+    h.push(2)
+    assert h.pop() == 1
+    assert h.pop() == 2
+    assert h.pop() == 3
+    assert not h
+    
