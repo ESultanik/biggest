3c973a39b2a296b462bc65d4d9841f919e64d51f
Evan Sultanik <evan@sultanik.com>
2018-11-05 09:27:16 -0500

    Only print the actual biggest to stdout

--- biggest/tree.py
+++ biggest/tree.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-def _print_single(obj, stdout, stderr, parent_indents, last, parent):
+def _print_single(obj, stdout, stderr, parent_indents, last, parent, biggest):
     for indent in parent_indents[:-1]:
         if indent:
             stderr.write('│   ')
@@ -20,18 +20,24 @@ def _print_single(obj, stdout, stderr, parent_indents, last, parent):
         basepath = obj.path[:-len(path)]
     else:
         basepath = ''
-    sys.stdout.write(f"{basepath}")
-    sys.stdout.flush()
+    if obj in biggest:
+        out_stream = sys.stdout
+    else:
+        out_stream = sys.stderr
+    out_stream.write(f"{basepath}")
+    out_stream.flush()
     sys.stderr.write('\x1B[1D \x1B[1D' * len(basepath))
     sys.stderr.flush()
-    sys.stdout.write(f"{path}\n")
-    sys.stdout.flush()    
+    out_stream.write(f"{path}\n")
+    out_stream.flush()    
 
-def _print_tree(directory, stdout, stderr, parent_indents=(), last=False, parent=None):
-    _print_single(directory, stdout, stderr, parent_indents, last, parent)
+def _print_tree(directory, stdout, stderr, parent_indents=(), last=False, parent=None, _biggest=None):
+    if _biggest is None:
+        _biggest = frozenset(directory.children)
+    _print_single(directory, stdout, stderr, parent_indents, last, parent, biggest=_biggest)
     for i, child in enumerate(directory.children):
         last_child = i == len(directory.children) - 1
-        _print_tree(child, stdout, stderr, parent_indents + ([True,False][last],), last=last_child, parent=directory)
+        _print_tree(child, stdout, stderr, parent_indents + ([True,False][last],), last=last_child, parent=directory, _biggest=_biggest)
 
 def print_tree(directory, stdout=sys.stdout, stderr=sys.stderr):
     _print_tree(directory, stdout, stderr)
