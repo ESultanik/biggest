294cd19e9c96b5c41dcacfc222e87d96db3ff03b
Evan Sultanik <evan@sultanik.com>
2018-11-05 09:17:25 -0500

    Use ANSI escapes instead of '\b' to clear the unwanted part of the path

--- biggest/tree.py
+++ biggest/tree.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-def _print_tree(directory, stdout, stderr, parent_indents=(), last=False, parent=None):
+def _print_single(obj, stdout, stderr, parent_indents, last, parent):
     for indent in parent_indents[:-1]:
         if indent:
             stderr.write('│   ')
@@ -12,18 +12,23 @@ def _print_tree(directory, stdout, stderr, parent_indents=(), last=False, parent
             sys.stderr.write('└── ')
         else:
             sys.stderr.write('├── ')
-    sys.stderr.write(f"[{directory.size}] ")
+    sys.stderr.write(f"[{obj.size}] ")
     sys.stderr.flush()
-    path = directory.path
+    path = obj.path
     if parent is not None:
         path = os.path.relpath(path, start=parent.path)
-    basepath = directory.path[:-len(path)]
+        basepath = obj.path[:-len(path)]
+    else:
+        basepath = ''
     sys.stdout.write(f"{basepath}")
     sys.stdout.flush()
-    sys.stderr.write('\b' * len(basepath))
+    sys.stderr.write('\x1B[1D \x1B[1D' * len(basepath))
     sys.stderr.flush()
     sys.stdout.write(f"{path}\n")
-    sys.stdout.flush()
+    sys.stdout.flush()    
+
+def _print_tree(directory, stdout, stderr, parent_indents=(), last=False, parent=None):
+    _print_single(directory, stdout, stderr, parent_indents, last, parent)
     for i, child in enumerate(directory.children):
         last_child = i == len(directory.children) - 1
         _print_tree(child, stdout, stderr, parent_indents + ([True,False][last],), last=last_child, parent=directory)
