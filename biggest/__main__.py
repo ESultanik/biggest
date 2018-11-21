de552f0a4708a597941bdc83e17a036aad713441
Evan Sultanik <evan@sultanik.com>
2018-11-21 14:46:37 -0500

    Removed a debug printout

--- biggest/__main__.py
+++ biggest/__main__.py
@@ -19,7 +19,7 @@ def main(argv = None):
     args = parser.parse_args(argv[1:])
 
     directory = biggest.Directory(args.directory, num_biggest=args.n, include_directories=not args.only_files)
-    print(f"{directory.biggest()}")
+
     for c in directory.biggest():
         c.selected = True
 
